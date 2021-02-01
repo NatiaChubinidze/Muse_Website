be9895f06b815a877ba66e0bc6094fc14fc0572b
LENOVO <chubinidze.natia97@gmail.com>
2021-02-01 13:32:30 +0400

    styling

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-const closeBtn=document.getElementById("close");
-let messageDiv=document.getElementById("message");
-const messageBtn=document.getElementById("messageBtn");
-const submitBtn=document.getElementById("submit");
+const closeBtn = document.getElementById("close");
+let messageDiv = document.getElementById("message");
+const messageBtn = document.getElementById("messageBtn");
+const submitBtn = document.getElementById("submit");
 
-
-closeBtn.addEventListener("click",(event)=>{
-messageDiv.classList.add("d-none");
-messageBtn.classList.remove("d-none");
+closeBtn.addEventListener("click", (event) => {
+  messageDiv.classList.add("d-none");
+  messageBtn.classList.remove("d-none");
 });
 
-messageBtn.addEventListener("click",()=>{
-    messageBtn.classList.add("d-none");
-    messageDiv.classList.remove("d-none");
+messageBtn.addEventListener("click", () => {
+  messageBtn.classList.add("d-none");
+  messageDiv.classList.remove("d-none");
 });
 
-submitBtn.addEventListener("click",()=>{
-    messageDiv.classList.add("d-none");
-    messageBtn.classList.remove("d-none");
+submitBtn.addEventListener("click", () => {
+  messageDiv.classList.add("d-none");
+  messageBtn.classList.remove("d-none");
 });
