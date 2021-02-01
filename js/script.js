c72136b31fb6060f614c44ee5541bd18fc30d02a
LENOVO <chubinidze.natia97@gmail.com>
2021-02-01 13:22:44 +0400

    adding features

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const closeBtn=document.getElementById("close");
+let messageDiv=document.getElementById("message");
+const messageBtn=document.getElementById("messageBtn");
+const submitBtn=document.getElementById("submit");
+
+
+closeBtn.addEventListener("click",(event)=>{
+messageDiv.classList.add("d-none");
+messageBtn.classList.remove("d-none");
+});
+
+messageBtn.addEventListener("click",()=>{
+    messageBtn.classList.add("d-none");
+    messageDiv.classList.remove("d-none");
+});
+
+submitBtn.addEventListener("click",()=>{
+    messageDiv.classList.add("d-none");
+    messageBtn.classList.remove("d-none");
+});
